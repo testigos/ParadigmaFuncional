401b1b38b4f5b48f8611e300869d8841f61ecfce
rbestardpino <49445152+rbestardpino@users.noreply.github.com>
2019-05-20 15:11:04 -0300

    hasta el 2 hecho

---
+++ MonstersIncPACO.hs
@@ -0,0 +1,45 @@
+import Text.Show.Functions
+
+type Monstruo = (Chico->Grito)
+
+data Grito = Grito {
+    onomatopeya::String,
+    intensidad::Int,
+    mojoLaCama::Bool
+} deriving (Show)
+
+data Chico = Chico {
+    nombre::String,
+    edad::Int,
+    altura::Float
+} deriving (Show)
+
+energiaDeGrito::Grito->Int
+energiaDeGrito grito 
+    | mojoLaCama grito = (length.onomatopeya) grito * (intensidad grito) ^ 2
+    | otherwise = 3 * (length.onomatopeya) grito + (intensidad grito)
+
+sullivan::Chico->Grito
+sullivan (Chico nom edad alt) = Grito ((repetir (length nom) 'A') ++ "GH") (div 20 edad) (edad < 3) 
+
+repetir::Int->a->[a]
+repetir 0 x = []
+repetir n x = x : repetir (n-1) x
+
+randall::Chico->Grito
+randall (Chico nom edad alt) = Grito ("Mamadera!") (cantVocales nom) (alt < 1.2 && alt > 0.8)
+
+cantVocales::String->Int
+cantVocales = (length.filter (esVocal))
+
+vocales = ['a','e','i','o','u']
+
+esVocal::Char->Bool
+esVocal letra = elem letra vocales
+
+chuck::Chico->Grito
+chuck (Chico nom edad alt) = Grito (['a'..'z']) 1000 True
+
+osito::Chico->Grito
+osito (Chico nom edad alt) = Grito "uf" edad False
+
